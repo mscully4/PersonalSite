374c6833221fd2f803a743542b1f90fed0ac1def
Mike <michael.scully1997@gmail.com>
2025-06-04 17:54:11 -0500

    Revert Temp API Key Change

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,12 +4,8 @@ import { data } from './data/resource';
 
 import { photoBucket } from './storage/resource';
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   photoBucket,
 });
-
-backend.data.resources.cfnResources.cfnApiKey?.overrideLogicalId(
-  `recoverApiKey${new Date().getTime()}`
-);
