84d87f47174bd2eb25c632eebe1a06f35daf10d5
Mike <michael.scully1997@gmail.com>
2025-06-04 17:42:18 -0500

    Temp API Key Rotation
    See: https://docs.amplify.aws/vue/build-a-backend/data/customize-authz/public-data-access/#rotate-an-api-key

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,8 +4,12 @@ import { data } from './data/resource';
 
 import { photoBucket } from './storage/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
   photoBucket,
 });
+
+backend.data.resources.cfnResources.cfnApiKey?.overrideLogicalId(
+  `recoverApiKey${new Date().getTime()}`
+);
